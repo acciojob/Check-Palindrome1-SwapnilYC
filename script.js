ff279bcb8292095ae181fe7faa3fc0933dc5de80
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-07 13:00:13 +0530

    some changes has been done

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // complete the given function
 
 function palindrome(str){
- str = str.toLowerCase();
+  str = str.toLowerCase();
     // Using Spread operator on string
     let arr = [...str];
     // console.log(arr);
     let arrCopy = [...str];    // as reverse method also make changes to original array
-    let arrReverse = arr.reverse();
-        arr = arrCopy;
-    console.log(`reverseArray:${arrReverse}`);
-    console.log(`array:${arr}`);
+    let arrReverse = arrCopy.reverse();
+    // arr = arrCopy;
+    // console.log(`reverseArray:${arrReverse}`);
+    // console.log(`original_array:${arr}`);
 
-     for (let i = 0; i < arr.length; i++) {
-        if (arrCopy[i] != arrReverse[i]) { 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != arrReverse[i]) {
             return false;
         }
     }
-
     // Directly looping over a string
     // var n = str.length;
     // for (let i = 0; i < n / 2; i++) {
@@ -24,6 +23,6 @@ function palindrome(str){
     //         return false;
     //     }
     // }
- return true;
+    return true;
 }
 module.exports = palindrome
